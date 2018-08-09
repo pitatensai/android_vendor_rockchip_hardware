aeab5b7b7f8b56f5845a883e41e01bb904a1534a
aisx <aisx@rock-chips.com>
2018-08-09 10:36:40 +0800

    1.format align 2.fix getCurMode & getCurColorMode error
    Change-Id: I2eafee335f26db776dcb1106ab65e530c1b2e8cf

--- interfaces/outputmanager/1.0/default/RkOutputManager.h
+++ interfaces/outputmanager/1.0/default/RkOutputManager.h
@@ -46,31 +46,31 @@ using ::android::sp;
 struct RkOutputManager : public IRkOutputManager {
     RkOutputManager(hw_output_device* hw_module);
     ~RkOutputManager();
-	Return<void> initial()  override;
 
-	Return<Result> setMode(Display display, const hidl_string& mode)  override;
-	Return<Result> setGamma(Display display, uint32_t size, const hidl_vec<uint16_t>& r, const hidl_vec<uint16_t>& g, const hidl_vec<uint16_t>& b) override;
-	Return<Result> setBrightness(Display display, uint32_t value) override;
-	Return<Result> setContrast(Display display, uint32_t value) override;
-	Return<Result> setSaturation(Display display, uint32_t value) override;
-	Return<Result> setHue(Display display, uint32_t value) override;
-	Return<Result> setScreenScale(Display display, uint32_t direction, uint32_t value) override;
-	Return<Result> setHdrMode(Display display, uint32_t hdrmode) override;
-	Return<Result> setColorMode(Display display, const hidl_string& mode)  override;
+    Return<void> initial()  override;
+    Return<Result> setMode(Display display, const hidl_string& mode)  override;
+    Return<Result> setGamma(Display display, uint32_t size, const hidl_vec<uint16_t>& r, const hidl_vec<uint16_t>& g, const hidl_vec<uint16_t>& b) override;
+    Return<Result> setBrightness(Display display, uint32_t value) override;
+    Return<Result> setContrast(Display display, uint32_t value) override;
+    Return<Result> setSaturation(Display display, uint32_t value) override;
+    Return<Result> setHue(Display display, uint32_t value) override;
+    Return<Result> setScreenScale(Display display, uint32_t direction, uint32_t value) override;
+    Return<Result> setHdrMode(Display display, uint32_t hdrmode) override;
+    Return<Result> setColorMode(Display display, const hidl_string& mode)  override;
 
-	Return<void> getCurCorlorMode(Display display, getCurCorlorMode_cb hidl_cb) override;
-	Return<void> getCurMode(Display display, getCurMode_cb _hidl_cb) override;
-	Return<void> getNumConnectors(Display display, getNumConnectors_cb _hidl_cb) override;
-	Return<void> getConnectState(Display display, getConnectState_cb _hidl_cb) override;
-	Return<void> getBuiltIn(Display display, getBuiltIn_cb _hidl_cb) override;
-	Return<void> getCorlorModeConfigs(Display display, getCorlorModeConfigs_cb _hidl_cb) override;
-	Return<void> getOverscan(Display display, getOverscan_cb _hidl_cb) override;
-	Return<void> getBcsh(Display display, getBcsh_cb _hidl_cb) override;
-	Return<void> getDisplayModes(Display display, getDisplayModes_cb _hidl_cb) override;
-	Return<void> saveConfig() override;
-	Return<void> hotPlug() override;
+    Return<void> getCurCorlorMode(Display display, getCurCorlorMode_cb hidl_cb) override;
+    Return<void> getCurMode(Display display, getCurMode_cb _hidl_cb) override;
+    Return<void> getNumConnectors(Display display, getNumConnectors_cb _hidl_cb) override;
+    Return<void> getConnectState(Display display, getConnectState_cb _hidl_cb) override;
+    Return<void> getBuiltIn(Display display, getBuiltIn_cb _hidl_cb) override;
+    Return<void> getCorlorModeConfigs(Display display, getCorlorModeConfigs_cb _hidl_cb) override;
+    Return<void> getOverscan(Display display, getOverscan_cb _hidl_cb) override;
+    Return<void> getBcsh(Display display, getBcsh_cb _hidl_cb) override;
+    Return<void> getDisplayModes(Display display, getDisplayModes_cb _hidl_cb) override;
+    Return<void> saveConfig() override;
+    Return<void> hotPlug() override;
 private:
-	hw_output_device* mHwOutput;
+    hw_output_device* mHwOutput;
 };
 
 extern "C" IRkOutputManager* HIDL_FETCH_IRkOutputManager(const char* name);
